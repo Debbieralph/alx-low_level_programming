7db1c84e6b0b0b7f87802c5a184a819f0de99a3a
Debbieralph <deborahrapheal@gmail.com>
2022-12-15 16:36:17 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int main(void)
 	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 ==0))
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf( " Fizz");
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0)
 		{
